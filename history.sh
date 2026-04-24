a4b8b37bcaf7058f509cb657051cf08ba4659485
FlavioG_2005 <151882583+FlavioS-G-2005@users.noreply.github.com>
2026-04-24 17:37:43 +0000

    Fix problem 2: files moved and renamed

---
+++ history.sh
@@ -0,0 +1,12 @@
+   Problema 1
+   9  mv kepler/docs kepler/registros
+   10  ls -R kepler
+   11  ls -F
+   12  mv evaluation/kepler/docs evaluation/kepler/registros
+   13  mkdir evaluation/kepler/plantillas
+   14  ls evaluation/kepler/
+   15  git add .
+   16  git commit -m "Fix problema 1: estructura de directorios corregida"
+   17  history
+   Problema 2
+   
