c3a8b0bfc697c53fad7428a81374676f28071f01
FlavioG_2005 <151882583+FlavioS-G-2005@users.noreply.github.com>
2026-05-28 13:45:28 +0000

    wip: grops

--- security2.zsh
+++ security2.zsh
@@ -7,13 +7,13 @@ touch ~/test_grupo_heredado.txt
 ls -la ~/test_grupo_heredado.txt
 #Lists detailed information (permissions, owner, group, size, and modification date) for the specified file, including hidden attributes.
 echo "Grupo actual :$(id -gn)"
-#
+#Prints a message displaying the name of the current user's active primary group.
 touch ~/antes_de_newgrp.txt
-#
+#Creates an empty file named antes_de_newgrp.txt in the user's home directory before any group changes are made.
 ls -la ~antes_de_newgrp.txt
-#
+#Displays the detailed attributes, including owner and group ownership, specifically for the newly created file.
 newgrp desarrolladores
-#
+#Changes the user's current primary group to "desarrolladores" for the remainder of the terminal session.
 ## Installing newgrp package
 apt update
 apt upgrade
@@ -31,3 +31,5 @@ ls -la ~/dentro_de_desarrolladores.txt
 ## Creating a new dir
 mkdir -p ~/proyecto_dev/src
 ls -la ~/
+proyecto_dev/ tiene grupo 'desarrolladores'
+#
