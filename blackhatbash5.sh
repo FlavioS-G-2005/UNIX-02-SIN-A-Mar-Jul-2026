212894cf20875aaf9638a58347b6079ef758a478
FlavioG_2005 <151882583+FlavioS-G-2005@users.noreply.github.com>
2026-06-05 16:49:16 +0000

    wip: comands background

---
+++ blackhatbash5.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+top
+# Displays a dynamic, real-time view of running system processes and resource usage.
+
+top&
+# Executes the 'top' command in the background, returning immediate control to the terminal.
